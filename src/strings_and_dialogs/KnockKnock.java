76fc07fd2a64cbc930d1ed94f5f416c55fb76b0f
Rey <rey.fidel@jointheleague.org>
2019-04-29 21:59:16 -0700

    additions

---
+++ src/strings_and_dialogs/KnockKnock.java
@@ -0,0 +1,16 @@
+package strings_and_dialogs;
+
+import javax.swing.JOptionPane;
+
+public class KnockKnock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,"Knock Knock");
+		JOptionPane.showMessageDialog(null,"Whos there");
+		JOptionPane.showMessageDialog(null,"Rey-Rey");
+		JOptionPane.showMessageDialog(null,"Rey-Rey Who?");
+		JOptionPane.showMessageDialog(null,"You can call me Rey, or You can call me Jay");
+	}
+
+}
---
+++ src/strings_and_dialogs/MorningZombie.java
@@ -0,0 +1,19 @@
+package strings_and_dialogs;
+
+import javax.swing.JOptionPane;
+
+public class MorningZombie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,"Turn off Alarm");
+		JOptionPane.showMessageDialog(null,"Turn on CD player");
+		JOptionPane.showMessageDialog(null,"Meditate");
+		JOptionPane.showMessageDialog(null,"Shower");
+		JOptionPane.showMessageDialog(null,"Brush Teeth");
+		JOptionPane.showMessageDialog(null,"Shave");
+		JOptionPane.showMessageDialog(null,"Do Hair");
+		JOptionPane.showMessageDialog(null,"Dress");
+	}
+
+}
